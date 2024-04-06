828298cfc310b8392d820df5a980bee3da330b32
MarekJames <hugombabreu2000@gmail.com>
2024-04-06 10:52:04 +0100

    Help Center - Fix frontend

--- AllWays/Views/Settings/Terms&Conditions.js
+++ AllWays/Views/Settings/Terms&Conditions.js
@@ -12,7 +12,7 @@ Terms&Conditions.js
 /******************** Imports Section ********************/ 
 
 import React from 'react';
-import { Text, View, TouchableOpacity, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from 'react-native';
 import  Ionicons  from '@expo/vector-icons/Ionicons';
 
 
@@ -27,34 +27,60 @@ import  Ionicons  from '@expo/vector-icons/Ionicons';
 */
 
 export class TermsConditionsScreen extends React.Component{
+  render() {  
+    return (
+      <View>
 
-    render() {  
-      return (
-        <View>
-            {/* Back Buttom */}
+          {/* Title and Back button */}
+          <View style = {{flexDirection:'row', marginTop:50, marginBottom:50}}>
             <TouchableOpacity
-                onPress={() => this.props.navigation.goBack()}
-                style={{
-                width: 45,
-                height: 45,
-                borderRadius: 30,
-                backgroundColor: 'lightgrey',
-                justifyContent: 'center',
-                alignItems: 'center',
-                marginLeft:15,
-                marginTop:40
-                }}
-            >
-                <Ionicons name="chevron-back-sharp" size={30} color="black" />
+                  onPress={() => this.props.navigation.goBack()}
+                  style={{
+                    width: 45,
+                    height: 45,
+                    borderRadius: 30,
+                    backgroundColor: '#fff',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    marginLeft:10
+                  }}
+                >
+                  <Text><Ionicons name="chevron-back-sharp" size={30} color="black" /></Text>
             </TouchableOpacity>
+            <Text style = {TermsConditionsStyles.title}>Terms & Conditions</Text>
+          </View>
 
-            {/* Scroll View with terms & conditions */}
-            <ScrollView style = {{alignSelf:'center'}}>
-                <Text>
-                    Terms & Conditions
-                </Text>
-            </ScrollView>
-        </View>
-      )    
-    }
+          {/* Scroll View with terms & conditions */}
+          <ScrollView style = {{alignSelf:'center'}}>
+              <Text>
+                  Text Here
+              </Text>
+          </ScrollView>
+      </View>
+    )    
   }
+}
+
+const TermsConditionsStyles = StyleSheet.create ({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor:'white'
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color:'#000000',
+    alignSelf:'center',
+    textAlign:'center',
+    flex:1,
+    marginRight:55
+  },
+  subTitle: {
+    fontSize: 20,
+    marginBottom: 30,
+    color:'#494949',
+    fontWeight:'600',
+    marginLeft:25
+  },
+})
