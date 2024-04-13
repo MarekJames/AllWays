f1960932743db8712d4ca9dcc041ee5813f90949
MarekJames <hugombabreu2000@gmail.com>
2024-04-13 01:17:30 +0100

    Profile - Clean up code

--- AllWays/Views/Settings/ChangeName.js
+++ AllWays/Views/Settings/ChangeName.js
@@ -12,10 +12,11 @@ ChangeName.js
 /******************** Imports Section ********************/ 
 
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, ImageBackground } from 'react-native';
-import { updateUser } from '../../config/firebase-config';
 import { Ionicons } from '@expo/vector-icons';
-import { getAuth } from '../../config/firebase-config';
+import { updateUser } from '../../config/firebase-config';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, ImageBackground } from 'react-native';
+
+
 
 
 /*********************** Classes *************************/
@@ -31,82 +32,99 @@ export class ChangeNameScreen extends React.Component{
   
   changeNameScreen = () => {
     
+    // Const to store name and error message
     const [name, setName] = useState('');
     const [invalidName, setInvalidName] = useState('');
   
+    // Function to handle submit button
     const handleSubmit = async (navigator) => {
         
-        if(!name){
-            setInvalidName('Please input your name');
+      // Check user inputs
+      if(!name){
+          setInvalidName('Please input your name');
+      }
+      if(name && invalidName){
+        setInvalidName('');
+      }
+      if(name){
+
+        // Call updateUser to chage name
+        const result = await updateUser(name);
+        if(result == 'success') {
+
+          // If successfull navigate to the Profile screen
+          navigator.navigate('Profile');
         }
-        if(name && invalidName){
-          setInvalidName('');
-        }
-        if(name){
-          const result = await updateUser(name);
-          if(result == 'success') {
-            navigator.replace('Profile', {userName : name});
-          }
-        }
+      }
     }
 
     return (
       <View style={ChangeNameStyles.container}>
 
+        {/* ImageBackground of the screen */}
         <ImageBackground
-          source={require('../../Images/LoginBackground.png')} // Replace with your image path
+          resizeMode="cover"
           style={ChangeNameStyles.imageBackground}
-          resizeMode="cover" // You can adjust the resizeMode property as needed
+          source={require('../../Images/LoginBackground.png')}
         >
-        
-        <View style = {{flexDirection:'row', marginTop:50, marginBottom:10}}>
-          <TouchableOpacity
-                onPress={() => this.props.navigation.goBack()}
-                style={{
-                  width: 45,
-                  height: 45,
-                  borderRadius: 30,
-                  backgroundColor: '#fff',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  marginLeft:10
-                }}
-              >
-                <Text><Ionicons name="chevron-back-sharp" size={30} color="black" /></Text>
+
+          {/* Header of the screen | Back button | Title */}
+          <View style = {{flexDirection:'row', marginTop:50, marginBottom:10}}>
+            
+            {/* Back button */}
+            <TouchableOpacity
+                  onPress={() => this.props.navigation.goBack()}
+                  style={{
+                    width: 45,
+                    height: 45,
+                    marginLeft:10,
+                    borderRadius: 30,
+                    alignItems: 'center',
+                    backgroundColor: '#fff',
+                    justifyContent: 'center'
+                  }}
+                >
+                  <Text><Ionicons name="chevron-back-sharp" size={30} color="black" /></Text>
+            </TouchableOpacity>
+
+            {/* Title */}
+            <Text style={ChangeNameStyles.title}>Change Name</Text>
+          </View>
+          
+          {/* Subtitle */}
+          <Text style={ChangeNameStyles.subTitle}>Enter your name</Text>
+
+          {/* Error Message */}
+          {invalidName !== null && ( // Checking if the variable is not null
+            <Text style = {{color:'red',fontSize:12,fontWeight:'600', textAlign:'center'}}>{invalidName}</Text>
+          )}
+
+          {/* Name Input */}
+          <TextInput
+            value={name}
+            placeholder="Name"
+            onChangeText={setName}
+            style={ChangeNameStyles.input}
+            placeholderTextColor={'#626262'}
+          />
+
+          {/* Submit Button */}
+          <TouchableOpacity style = {ChangeNameStyles.recover} onPress={() => {handleSubmit(this.props.navigation)}}>
+            <Text style = {{fontSize:20, fontWeight:'600', textAlign:'center', color:'#FFFFFF'}}>Update</Text>
           </TouchableOpacity>
-
-          <Text style={ChangeNameStyles.title}>Change Name</Text>
-        </View>
-
-        <Text style={ChangeNameStyles.subTitle}>Enter your name</Text>
-
-        {invalidName !== null && ( // Checking if the variable is not null
-          <Text style = {{color:'red',fontSize:12,fontWeight:'600', textAlign:'center'}}>{invalidName}</Text>
-        )}
-        <TextInput
-          style={ChangeNameStyles.input}
-          placeholder="Name"
-          placeholderTextColor={'#626262'}
-          value={name}
-          onChangeText={setName}
-        />
-
-        <TouchableOpacity style = {ChangeNameStyles.recover} onPress={() => {handleSubmit(this.props.navigation)}}>
-          <Text style = {{fontSize:20, fontWeight:'600', textAlign:'center', color:'#FFFFFF'}}>Update</Text>
-        </TouchableOpacity>
-  
+    
         </ImageBackground>
       </View>
     );
   };
 
+  // Render screen
   render() {  
     return (
       <this.changeNameScreen></this.changeNameScreen>
     )    
   }
 }
-
 
 
 
@@ -120,68 +138,43 @@ const ChangeNameStyles = StyleSheet.create ({
     backgroundColor:'white'
   },
   imageBackground:{
-      flex: 1,
-      width: '100%', // You can adjust width and height as needed
-      height: '100%',
+    flex: 1,
+    width: '100%',
+    height: '100%',
   },
   title: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    color:'#000000',
-    alignSelf:'center',
-    textAlign:'center',
     flex:1,
-    marginRight:55
-
+    fontSize: 30,
+    marginRight:55,
+    color:'#000000',
+    fontWeight: 'bold',
+    alignSelf:'center',
+    textAlign:'center'
   },
   subTitle: {
     fontSize: 20,
-    marginBottom: 40,
     color:'#494949',
     fontWeight:'600',
+    marginBottom: 40,
     textAlign:'center'
   },
   input: {
-    width: '80%',
     height: 50,
+    width: '80%',
     paddingLeft: 20,
-    marginVertical: 30,
     borderRadius:30,
     alignSelf:'center',
+    marginVertical: 30,
     backgroundColor:'#F1F4FF'
   },
-  alreadyHaveAccount: {
-    textAlign: 'center',
-    fontSize: 14,
-    marginTop: 20,
-    fontWeight:'600',
-    color:'#494949'
-  },
   recover: {
-    width: '80%',
     height: 50,
-    backgroundColor:'#23C2DF',
+    width: '80%',
     marginTop:10,
-    marginBottom: 10,
     borderRadius:30,
+    marginBottom: 10,
     alignSelf:'center',
-    justifyContent:'center'
-  },
-  continueWith:{
-    textAlign:'center',
-    fontSize:14,
-    color:'#23C2DF',
-    fontWeight:'600',
-    marginTop:30
-  },
-  icons:{
-    alignItems:'center', 
-    justifyContent:'center', 
-    width:60, 
-    height:50, 
-    borderRadius: 10, 
-    backgroundColor:'#ECECEC',
-    marginHorizontal:10
+    justifyContent:'center',
+    backgroundColor:'#23C2DF'
   }
- 
 })
